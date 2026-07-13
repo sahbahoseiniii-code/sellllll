d1fa0b1f6627881d2d15651c18f6cc0844c34d2b
sahbahoseiniii-code <sahbahoseiniii@gmail.com>
2026-07-14 01:28:17 +0330

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { AuthProvider } from './contexts/AuthContext';
 import { CartProvider } from './contexts/CartContext';
 import { ToastProvider } from './contexts/ToastContext';
+import './index.css';
 
-export default function Main() {
-  return (
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
         <ToastProvider>
@@ -16,5 +18,5 @@ export default function Main() {
         </ToastProvider>
       </AuthProvider>
     </BrowserRouter>
-  );
-}
+  </React.StrictMode>
+);
